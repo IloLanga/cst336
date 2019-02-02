acc9f32780798bf79038bc955698d72953ce8f28
Ilona Poilliot <ipoilliot@csumb.edu>
2019-02-02 00:26:28 +0000

    change main page 2

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html><body>
+    <a href="/labs/lab1/index.html">Lab1 Portfolio</a>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
