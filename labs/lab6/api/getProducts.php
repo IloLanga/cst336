02801b0090c0a2747ec9f6895b750e82efd92a57
Ilona Poilliot <ipoilliot@csumb.edu>
2019-03-09 23:40:20 +0000

    lab5 username check in password in progress

--- labs/lab6/api/getProducts.php
+++ labs/lab6/api/getProducts.php
@@ -1,23 +1,24 @@
 <?php
 
-$host = "localHost"; //cloud9 database. this is the url to the database
-$dbname ="ottermart";
+$host = "localhost";
+$dbname = "ottermart";
 $username = "root";
 $password = "";
 
-//establish connection
+// Establishing a connection
 $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 // Setting Errorhandling to Exception
 $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-//to retreive data
+
 $sql = "SELECT * FROM om_product ORDER BY productPrice LIMIT 10";
-$stmt = $dbConn -> prepare($sql);  //$connection MUST be previously initialized --> here we changed it to dbConn
+$stmt = $dbConn -> prepare($sql);  //$connection MUST be previously initialized
 $stmt->execute();
 $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
 
-//print_r($records); //display array content
+//print_r($records); //displays array content
 
 echo json_encode($records);
 
+//echo $records[0]['productName'];
 ?>
